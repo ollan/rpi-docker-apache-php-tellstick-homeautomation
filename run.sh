2fdf874d3cccf9940befa2f3efc6383fd0f93cc8
Johan Axfors <johan@axfors.se>
2017-01-28 23:03:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# install app
+[[ ! -d "/app/.svn" ]] && \
+	svn co svn://karpero.mine.nu/homeautomation /app
+ 
+# update
+svn update /app
+
+# excutable
+find /app -iname "*.php" | xargs chmod +x
+
+#test
+echo test >> /app/test.txt
+
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
